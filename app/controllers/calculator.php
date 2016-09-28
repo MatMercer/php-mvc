7080eaafccfaef422395a9381b492f27a5c03ddd
MrAnyone <m3rc3r99@gmail.com>
2016-09-28 16:31:07 -0300

    Add view to calculator controller

--- app/controllers/calculator.php
+++ app/controllers/calculator.php
@@ -2,11 +2,10 @@
 
 class Calculator extends Controller {
     public function index() {
-        echo 'Hey! This is a calculator! It still only sum 2 numbers... Yet you need to use the URL to pass the parameters. ';
-        echo 'Example: URL/calculator/sum/3/5';
+        $this->view('calculator/index', []);
     }
 
     public function sum($x = 2, $y = 3) {
-        echo $x . ' + ' . $y . ' = ' . ($x + $y);
+        $this->view('calculator/sum', ['x' => $x, 'y' => $y, 'result' => $x + $y]);
     }
 }
