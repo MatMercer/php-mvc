46070e2f48229cf85118be98924073636938d42f
MrAnyone <m3rc3r99@gmail.com>
2016-09-28 10:49:41 -0300

    Add attributes and methods to App.php

--- app/core/App.php
+++ app/core/App.php
@@ -1,4 +1,26 @@
 <?php
 
 class App {
+    // default controller and methods
+    protected $controller = 'home';
+    protected $method = 'index';
+
+    // parameters passed in the url
+    protected $params = [];
+
+    public function __construct() {
+        // echo 'App instantiated';
+        $this->parseUrl();
+    }
+
+    /*
+     * it parses the url based in the
+     * url parameter, an example of url is
+     * http://localhost/public/?url=something
+     */
+    public function parseUrl() {
+        if(isset($_GET['url'])) {
+            echo $_GET['url'];
+        }
+    }
 }
