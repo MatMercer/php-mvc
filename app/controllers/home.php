9cdaae65b4ddd3a333f094f224495190a2f7610c
MrAnyone <m3rc3r99@gmail.com>
2016-09-28 16:30:46 -0300

    Add view to home controller

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,7 +3,7 @@
 class Home extends Controller {
     public function index($name = 'Anonymous Kitty') {
         $user = $this->model('User');
-        $user->name = $name;
-        echo 'Welcome to home page ' . $user->name . '.';
+        $user->name = ucfirst($name);
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
