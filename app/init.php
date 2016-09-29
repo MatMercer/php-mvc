d924edeba0df6d852ebcab1c43f414ca1e30ae33
MrAnyone <m3rc3r99@gmail.com>
2016-09-29 09:38:36 -0300

    Add composer settings

--- app/init.php
+++ app/init.php
@@ -1,6 +1,10 @@
 <?php
 
+// composer autoloader
+require_once '../vendor/autoload.php';
+
 // load the app
-require_once('core/App.php');
+require_once 'core/App.php';
+
 // load the controller
-require_once('core/Controller.php');
+require_once 'core/Controller.php';
