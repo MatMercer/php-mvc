4e1e953f993ea5d17759c0bd970f88ee6c05621d
MrAnyone <m3rc3r99@gmail.com>
2016-09-30 10:35:27 -0300

    Add eloquent capsule database configuration

--- app/database.php
+++ app/database.php
@@ -1,2 +1,20 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule();
+
+// connection settings
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'username' => 'mranyone',
+    'password' => 'myspass',
+    'database' => 'php-mvc',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_cli',
+    'prefix' => ''
+]);
+
+// load eloquent based on the settings
+$capsule->bootEloquent();
